4e7d36aa6933f342fbe169ca2450c11778b61241
Jiya_agrawal. <jiyaagrawal6395@gmail.com>
2024-12-22 22:50:07 +0530

    Add files via upload

---
+++ Javascript/video_3/index.js
@@ -0,0 +1,19 @@
+console.log("Hey i am jiya agrawal")
+
+let age=18;
+//let fun=3;
+/*
+console.log(age+fun);
+console.log(age-fun);
+console.log(age*fun);
+console.log(age/fun);
+console.log(age**fun);
+console.log(age%fun);
+*/
+if(age==18){
+    console.log("You can drive");
+}
+
+else{
+    console.log("You can not drive");
+}
