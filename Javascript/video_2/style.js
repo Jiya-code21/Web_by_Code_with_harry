0309002ef124f8ea928445a9329c3dd9f3a016c8
Jiya_agrawal. <jiyaagrawal6395@gmail.com>
2024-12-22 22:23:26 +0530

    Add files via upload

---
+++ Javascript/video_2/style.js
@@ -0,0 +1,10 @@
+console.log("Hey this is tutorial 2")
+/* result is  shown in console in inspect part*/
+var a=5;
+var b=6;
+var c="Jiya";
+console.log(a+b+8)
+console.log(typeof a,typeof c)
+
+const a1=7;
+a1=a1+1;
