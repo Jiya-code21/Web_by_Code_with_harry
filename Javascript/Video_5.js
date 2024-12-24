ef5d88371ccc8f0d12ac0280c9e765c91a3ef3ed
Jiya_agrawal. <jiyaagrawal6395@gmail.com>
2024-12-24 16:35:06 +0530

    Create Video_5.js

---
+++ Javascript/Video_5.js
@@ -0,0 +1,25 @@
+console.log("This is strings tutorial")
+let a="Jiya";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+//console.log(a[4])
+
+//javascript is a forgiving language it gives less error.
+
+console.log(a.length)
+
+let real_name="Jiya"
+let friend="Deepika"
+//template literals
+
+console.log(`His name is ${real_name} and his friends name is ${friend}`)
+
+let b="Jiya"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,5))
+console.log(b.replace("iy","21"))
+console.log(b.concat(friend))
